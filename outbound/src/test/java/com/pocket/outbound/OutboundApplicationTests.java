1738582815644d4c8a8e11c2131612058be0bc8e
안정후 <austinan@naver.com>
2024-09-29 20:14:26 +0900

    feat: outbound module settings

---
+++ outbound/src/test/java/com/pocket/outbound/OutboundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket.outbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
