d6dd5e3a85b20f5846aae89236d4288051a750b6
안정후 <austinan@naver.com>
2024-09-29 20:13:56 +0900

    feat: hexagonal module settings

---
+++ hexagonal/src/main/java/com/chawoomi/hexagonal/HexagonalApplication.java
@@ -0,0 +1,18 @@
+package com.pocket.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.chawoomi")
+@ComponentScan(basePackages = "com.chawoomi")
+public class HexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalApplication.class, args);
+    }
+
+}
+
